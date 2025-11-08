4cef405c7d7c47eb09a2dbd01ae665e84b081b28
Tomas Tamadamas <1058433+tamadamas@users.noreply.github.com>
2025-11-08 13:52:36 +0100

    Read input and print error

--- lib/main.ex
+++ lib/main.ex
@@ -1,5 +1,13 @@
 defmodule CLI do
   def main(_args) do
-    IO.write("$ ")
+    run_loop()
+  end
+
+  defp run_loop do
+    user_input = IO.gets("$ ") |> String.trim_trailing
+    
+    IO.puts(user_input <> ": command not found")
+
+    run_loop()
   end
 end
